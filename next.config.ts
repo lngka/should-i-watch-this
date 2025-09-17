4e6928b557692dbc07d6e313848f53ae67d936d4
Ck. Nguyen <nvckhoa@gmail.com>
2025-09-17 16:20:29 +0200

    chore: vercel configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Keep eslint in dev, but don't block production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
